bc50bccbedc8909f43f67b8ff53988f854c99bfb
Patrick Lai <patrick.lai@lifeletters.com.au>
2016-07-25 16:16:05 +1000

    Made app listen

--- web.js
+++ web.js
@@ -51,6 +51,8 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:8080');
+app.listen(process.env.PORT || 5000);
+
+http.listen(5000, function(){
+  console.log('listening on *:5000');
 });
